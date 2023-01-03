7bf8f0196df8166c1402a4398a93778c42532095
EfJot321 <115103209+EfJot321@users.noreply.github.com>
2023-01-03 19:30:07 +0100

    evolution-wersja koncowa

--- Evolution/src/main/java/agh/ics/oop/Animal.java
+++ Evolution/src/main/java/agh/ics/oop/Animal.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Random;
 
 public class Animal extends AbstractWorldMapElement{
-//    private MapDirection dir;
     private Vector2d pos;
 
     private IWorldMap map;
